91ebd1c66a2567484b7f4a25ee8c159d106c36a0
mHamzaHanif <hamzaarain1999@gmail.com>
2020-11-01 05:47:24 +0500

    args biconditining

--- args.py
+++ args.py
@@ -22,7 +22,7 @@ class CLIArguments():
         )
         parser.add_argument(
             "-mmode" ,'--msg_mode',
-            choices=("both, cli, file, no"),
+            choices=("both, cli, file, none"),
             help='Msg send to targeted recipients.'
         )
 
@@ -35,7 +35,7 @@ class CLIArguments():
         
         parser.add_argument(
             '-amode','--attach_mode',
-            choices=("both, cli, file, no"),
+            choices=("both, cli, file, none"),
             help='''File/s to import.\
                 That will send to all targeted recipients'''
         )
@@ -70,7 +70,7 @@ class CLIArguments():
             return self.args.attach_files.split(",")
         return None
 
-    def modes(self):
+    def explicit_modes(self):
         if (self.args.msg_mode != None) and (self.args.attach_mode != None):
             return [self.args.msg_mode, self.args.attach_mode]
         elif (self.args.msg_mode != None) and (self.args.attach_mode == None):
@@ -78,6 +78,49 @@ class CLIArguments():
         elif (self.args.msg_mode == None) and (self.args.attach_mode != None):
             return [None, self.args.attach_mode]
         return [None, None]
+
+    def implicit_modes(self):
+        senders = self.recipient_arg()
+        msg = self.msg_arg()
+        attach = self.attach_file_arg() 
+
+        ## "both" mode
+        # files(insides msgs) + CLI msg
+        if (None not in senders[2]) and (msg != None):
+            self.args.msg_mode = "both"
+
+        # files(insides attachments) + CLI attachments
+        if (None not in senders[2]) and (None not in attach):
+            self.args.attach_mode = "both"
+
+        ## "cli"  mode
+        # No files + cli msg
+        if (None in senders[2]) and (msg != None):
+            self.args.msg_mode = "cli"
+
+        # No files + cli attach
+        if (None in senders[2]) and (None not in attach):
+            self.args.attach_mode = "cli"
+
+        ## 'file' mode
+        # Files + no cli msg
+        if (None not in senders[2]) and (msg == None):
+            self.args.msg_mode = "file"
+
+        # Files + no cli attaches
+        if (None not in senders[2]) and (None in attach):
+            self.args.attach_mode = "files"
+
+        ## "none" mode
+        # No file + no cli msg
+        if (None in senders[2]) and (msg == None):
+            self.args.msg_mode = "none"
+
+        # No file + no cli attachments
+        if (None in senders[2]) and (None in attach):
+            self.args.attach_mode = "none"
+
+
 
 if __name__ == "__main__":
     cli_args =  CLIArguments()  
@@ -91,3 +134,25 @@ if __name__ == "__main__":
 
 # python3 args.py -r "hamza,Linear aljebra,923370392561,923342843869,file.csv,o.xlsx" -m "Hi" -amode both -af 1.png,2.png -mmode both
 # ['both', 'both'] [[923370392561, 923342843869], ['hamza', 'Linear aljebra'], ['file.csv', 'o.xlsx']] Hi ['1.png', '2.png']
+
+"""ARGS
+REQUIRED
+--------
+-r  recipients <list of names, numbers, files>
+
+OPTIONAL
+--------
+-m  message <String>
+-mmode msg_mode ("both", "cli", "file", "none")
+
+-at attachment_files <list of files>
+-amode attach_mode ("both", "cli", "file", "none")
+
+DEFAULT MODES
+-------------
+both: when file/s & <numbers/names>
+cli: only cli msg(s)/attachment(s) even file is given
+file: only applicable when file is given
+none: no msg(s)/attachmet(s) even cli/file args given
+
+"""
--- script.py
+++ script.py
@@ -6,7 +6,7 @@ import numpy as np
 from args import CLIArguments 
 
 class WhatsappBot():
-    def __init__(self, modes, recipients, message, attchments):
+    def __init__(self, modes, recipients, message, attchments, web_driver_path):
         pass
 
 
@@ -49,13 +49,41 @@ class WhatsappBot():
 if __name__ == "__main__":
     cli_args =  CLIArguments()  
 
-    recipients = cli_args.recipient_arg()
-    message = cli_args.msg_arg()
-    attachments= cli_args.attach_file_arg() 
-
+    RECIPIENTS = cli_args.recipient_arg()
+    MESSAGE = cli_args.msg_arg()
+    ATTACHMENTS= cli_args.attach_file_arg() 
+    MODES = cli_args.modes()
     print(f"{recipients} {message} {attachments}")
 
-
-
-    # w = WhatsappBot(modes, recipients, message, attchments)
+    WEB_DRIVER_PATH = "/media/hamza/linux1/Coding/Python/whatsapp_bulk_msg_sender/geckodriver" 
+    w = WhatsappBot(modes=MODES,
+                recipients=RECIPIENTS,
+                message=MESSAGE,
+                attchments=ATTACHMENTS,
+                web_driver_path=WEB_DRIVER_PATH)
     # w.run()
+
+# python3 args.py -r "hamza,Linear aljebra,923370392561,923342843869,file.csv,o.xlsx" -m "Hi" -amode both -af 1.png,2.png -mmode both
+# ['both', 'both'] [[923370392561, 923342843869], ['hamza', 'Linear aljebra'], ['file.csv', 'o.xlsx']] Hi ['1.png', '2.png']
+
+"""ARGS
+REQUIRED
+--------
+-r  recipients <list of names, numbers, files>
+
+OPTIONAL
+--------
+-m  message <String>
+-mmode msg_mode ("both", "cli", "file", "none")
+
+-at attachment_files <list of files>
+-amode attach_mode ("both", "cli", "file", "none")
+
+DEFAULT MODES
+-------------
+both: when file/s & <numbers/names>
+cli: only cli msg(s)/attachment(s) even file is given
+file: only applicable when file is given
+none: no msg(s)/attachmet(s) even cli/file args given
+
+"""
